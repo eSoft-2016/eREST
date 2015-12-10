28e21241e20880ac98a9bd318d3e91fd1fb764fe
Roberto2504 <robertoaf19@gmail.com>
2015-12-09 21:08:29 -0600

    Aplicación
    lo único que ha echo flores

---
+++ Aplicacion/eREST.BO/ModelDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eREST.BO
+{
+    public partial class ModelDataContext
+    {
+
+        //public ModelDataContext() :
+        //    base(global::SGVCV.BO.Properties.Settings.Default.sgv_chica_veranoConnectionString, mappingSource)
+        //{
+        //    OnCreated();
+        //}
+
+    }
+}
