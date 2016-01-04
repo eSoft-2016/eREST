808239a92b543fc786d65cbb2df5627b07c4fd60
Luis Barrantes <luis1456.3@gmail.com>
2016-01-04 13:49:15 -0600

    Mantenimiento de empleados
    Mejora 1.2

--- eREST software/eREST/eREST.BL/Empleado/MantenimientoEmpleado.cs
+++ eREST software/eREST/eREST.BL/Empleado/MantenimientoEmpleado.cs
@@ -11,7 +11,6 @@ namespace eREST.BL.Empleado
 {
     public class MantenimientoEmpleado
     {
-<<<<<<< HEAD
         public void RegistrarEmpleado(eREST_PERSONA pPersona)
         {
             eREST_DiagramaDataContext dc = new eREST_DiagramaDataContext();
@@ -25,20 +24,6 @@ namespace eREST.BL.Empleado
             nuevoEmpleado.EMP_FECHAREGISTRO = diaYhora.Date;
 
             dc.eREST_EMPLEADOs.InsertOnSubmit(nuevoEmpleado);
-=======
-        public void RegistrarEmpleado(eREST_PERSONA pPersona, eREST_EMPLEADO pEmpleado)
-        {
-            eREST_DiagramaDataContext dc = new eREST_DiagramaDataContext();
-            MantenimientoPersona nuevaPersona = new MantenimientoPersona();
-            nuevaPersona.RegistrarPersona(pPersona);
-
-            pEmpleado.EMP_FK_PERSONA = pPersona.PER_PK_PERSONA;
-            pEmpleado.EMP_FK_TIPOEMPLEADO = 1;
-            var diaYhora = DateTime.Now;
-            pEmpleado.EMP_FECHAREGISTRO = diaYhora.Date;
-
-            dc.eREST_EMPLEADOs.InsertOnSubmit(pEmpleado);
->>>>>>> origin/master
             dc.SubmitChanges();
         }
     }
